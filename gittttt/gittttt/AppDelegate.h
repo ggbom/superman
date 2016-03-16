fe489d41edbcf5818dd89b829019f4f8e86af289
djh <82749941@qq.com>
2016-03-16 10:16:37 +0800

    搭建框架

---
+++ gittttt/gittttt/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  gittttt
+//
+//  Created by lanou on 16/3/16.
+//  Copyright © 2016年 邓健豪. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ gittttt/gittttt/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  gittttt
+//
+//  Created by lanou on 16/3/16.
+//  Copyright © 2016年 邓健豪. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
